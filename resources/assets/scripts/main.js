9719a8b9afe62cd43584781a5a6b522b4c92f914
Tim Albert <timothy.m.albert@gmail.com>
2017-09-01 16:31:36 -0400

    Hide kernl imports for now

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,6 +1,6 @@
 // import external dependencies
 import 'jquery';
-import 'kernl-ui/resources/assets/scripts/kernl';
+// import 'kernl-ui/resources/assets/scripts/kernl';
 
 // Import everything from autoload
 import "./autoload/**/*"
