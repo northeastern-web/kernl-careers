79a1ad5d2244f3b799641a3a7ba9e4303a33ebcd
Tim Albert <timothy.m.albert@gmail.com>
2017-09-15 10:18:32 -0400

    Update card, tabs, related

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,21 +1,22 @@
 <article {{ post_class('card') }}>
-  <a href="{{ the_permalink() }}" class="card__link">
-    <header class="card__header">
-      <div class="card__column"><div class="badge badge--{{ (get_field('radio_color', 'category_'.get_the_category()[0]->term_id) ? get_field('radio_color', 'category_'.get_the_category()[0]->term_id) : 'transparent' ) }}">{{ get_the_category()[0]->name }}</div></div>
-      {{-- <div class="card__column"><div class="badge badge--transparent">Video</div></div> --}}
+  <a href="{{ the_permalink() }}" class="__link">
+    <header class="__header">
+      @if (get_the_category())
+        <div class="__column"><div class="badge badge--{{ (get_field('radio_color', 'category_'.get_the_category()[0]->term_id) ? get_field('radio_color', 'category_'.get_the_category()[0]->term_id) : 'transparent' ) }}">{{ get_the_category()[0]->name }}</div></div>
+      @endif
     </header>
 
     @if (has_post_thumbnail())
-      <div class="card__graphic">
-        <img class="card__graphic__img" alt="{{ the_title() }}"
+      <div class="__graphic">
+        <img class="__graphic__img" alt="{{ the_title() }}"
           src="{{ the_post_thumbnail_url('large') }}"
           data-src="{{ the_post_thumbnail_url('large') }}">
       </div>
     @endif
 
-    <section class="card__body">
-      <h2 class="card__title">{{ the_title() }}</h2>
-      <div class="card__excerpt">
+    <section class="__body">
+      <h2 class="__title">{{ the_title() }}</h2>
+      <div class="__excerpt">
         @if (has_excerpt())
           {{ get_the_excerpt() }}
         @else
@@ -23,9 +24,9 @@
         @endif
       </div>
     </section>
-    <footer class="card__footer">
-      <div class="card__column">by <b>{{ get_the_author() }}</b></div>
-      <div class="card__column">&raquo;</div>
+    <footer class="__footer">
+      <div class="__column">by <b>{{ get_the_author() }}</b></div>
+      <div class="__column">&raquo;</div>
     </footer>
   </a>
 </article>
--- resources/views/single-article.blade.php
+++ resources/views/single-article.blade.php
@@ -7,7 +7,7 @@
 
       <section class="section">
         <div class="row">
-          <div class="col --12@xs">
+          <div class="col --12@xs --9@md">
             @while((have_rows('lay_section')))
               @php(the_row())
 
@@ -17,11 +17,35 @@
 
               @else
                 <p><i>Tabbed Section</i></p>
-                <h4>{{ the_sub_field('txt_title') }}</h4>
-                {{ the_sub_field('txt_copy') }}
+                @while((have_rows('lay_tab')))
+                  @php(the_row())
+                  <h4>{{ the_sub_field('txt_title') }}</h4>
+                  {{ the_sub_field('txt_copy') }}
+                @endwhile
 
               @endif
             @endwhile
+          </div>
+          <div class="col --12@xs --3@md">
+            <article class="card --scroll +list-group">
+              <header class="__header">
+                <div class="__column">
+                  <div>Related Articles</div>
+                </div>
+              </header>
+              <div class="__body">
+                <div class="__excerpt">
+                  <ul class="list-group">
+                    <li>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</li>
+                    <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+                    <li>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</li>
+                    <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+                    <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+                    <li>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</li>
+                  </ul>
+                </div>
+              </div>
+            </article>
           </div>
         </div>
       </section>
