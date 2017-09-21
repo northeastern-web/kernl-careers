ea8da8ff2e740025f71a28ba47ea34edc6034d4c
Tim Albert <timothy.m.albert@gmail.com>
2017-09-21 17:08:53 -0400

    More pieces of the puzzle

--- resources/views/single-article.blade.php
+++ resources/views/single-article.blade.php
@@ -12,20 +12,36 @@
               @php(the_row())
 
               @if(get_row_layout() == 'lay_basic')
-                <p><i>Basic Section</i></p>
                 {{ the_sub_field('txt_copy') }}
 
               @else
-                <p><i>Tabbed Section</i></p>
-                @while((have_rows('lay_tab')))
-                  @php(the_row())
-                  <h4>{{ the_sub_field('txt_title') }}</h4>
-                  {{ the_sub_field('txt_copy') }}
-                @endwhile
-
+                <div class="nav --tabbed">
+                  <ul class="__list" role="tablist">
+                    @php($i = 0)
+                    @while((have_rows('lay_tab')))
+                      @php(the_row())
+                      <li class="__item">
+                        <a class="__link {{ ($i == 0 ? 'active' : '') }}" data-toggle="tab" href="#tab_{{ $i }}" role="tab" aria-expanded="true">{{ the_sub_field('txt_title') }}</a>
+                      </li>
+                      @php($i++)
+                    @endwhile
+                  </ul>
+                  <div class="tab-content">
+                    @php($i = 0)
+                    @while((have_rows('lay_tab')))
+                      @php(the_row())
+                      <div class="tab-pane {{ ($i == 0 ? 'active' : '') }}" id="tab_{{ $i }}" role="tabpanel">
+                        @include('partials.articles.actions')
+                        {{ the_sub_field('txt_copy') }}
+                      </div>
+                      @php($i++)
+                    @endwhile
+                  </div>
+                </div>
               @endif
             @endwhile
           </div>
+
           <div class="col --12@xs --3@md">
             <article class="card --scroll +list-group">
               <header class="__header">
