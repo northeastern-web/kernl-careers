e050abf49e61c7c0033d05b644d0e6a7a1469713
Tim Albert <timothy.m.albert@gmail.com>
2017-09-29 10:43:50 -0400

    Edit action templates

---
+++ resources/views/partials/articles/related.blade.php
@@ -0,0 +1,19 @@
+<article class="card --scroll +list-group">
+  <header class="__header">
+    <div class="__column">
+      <div>Related Articles</div>
+    </div>
+  </header>
+  <div class="__body">
+    <div class="__excerpt">
+      <ul class="list-group">
+        <li>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</li>
+        <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+        <li>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</li>
+        <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+        <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+        <li>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</li>
+      </ul>
+    </div>
+  </div>
+</article>
--- resources/views/single-article.blade.php
+++ resources/views/single-article.blade.php
@@ -32,6 +32,7 @@
                       @php(the_row())
                       <div class="tab-pane {{ ($i == 0 ? 'active' : '') }}" id="tab_{{ $i }}" role="tabpanel">
                         @include('partials.articles.actions')
+
                         {{ the_sub_field('txt_copy') }}
                       </div>
                       @php($i++)
@@ -43,25 +44,7 @@
           </div>
 
           <div class="col --12@xs --3@md">
-            <article class="card --scroll +list-group">
-              <header class="__header">
-                <div class="__column">
-                  <div>Related Articles</div>
-                </div>
-              </header>
-              <div class="__body">
-                <div class="__excerpt">
-                  <ul class="list-group">
-                    <li>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</li>
-                    <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
-                    <li>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</li>
-                    <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
-                    <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
-                    <li>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</li>
-                  </ul>
-                </div>
-              </div>
-            </article>
+            @include('partials.articles.related')
           </div>
         </div>
       </section>
