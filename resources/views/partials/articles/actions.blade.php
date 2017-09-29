c72f23f0ed8cb68033aafbb45220044d9064cb1e
Tim Albert <timothy.m.albert@gmail.com>
2017-09-29 12:47:10 -0400

    Update actions and related articles

--- resources/views/partials/articles/actions.blade.php
+++ resources/views/partials/articles/actions.blade.php
@@ -1,19 +1,15 @@
 @if(have_rows('lay_actions'))
-  <aside class="alert --outline f--r@sm col --3@sm">
-    @while(have_rows('lay_actions'))
-      @php(the_row())
-      <h6>Actions</h6>
-      @php(var_dump(get_sub_field('rel_action')))
-      <ul>
-        @if(get_sub_field('rel_action'))
-          <li>{{ get_sub_field('rel_action')->post_title }}</li>
-        @else
+  <aside class="alert --outline f--r@sm col --3@sm pa--0@xs">
+    <div class="__body">
+      @while(have_rows('lay_actions'))
+        @php(the_row())
+        <ul class="list--unstyled">
           <li>
+            {!! (get_sub_field('rel_action')->name ? "<h6>". get_sub_field('rel_action')->name .'</h6>' : '') !!}
             <a href="{{ get_sub_field('txt_url') }}">{{ get_sub_field('txt_title') }}</a><br>
-            {{ get_sub_field('txt_description') }}
           </li>
-        @endif
-      </ul>
-    @endwhile
+        </ul>
+      @endwhile
+    </div>
   </aside>
 @endif
