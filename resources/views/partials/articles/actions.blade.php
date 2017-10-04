0dfd7ab1ff9d3f8e48559f84bb9a9e37e2a2386d
Tim Albert <timothy.m.albert@gmail.com>
2017-10-04 16:59:47 -0400

    Style actions box

--- resources/views/partials/articles/actions.blade.php
+++ resources/views/partials/articles/actions.blade.php
@@ -1,9 +1,10 @@
 @if(have_rows('lay_actions'))
-  <aside class="alert --blue-light f--r@sm col --12@sm pa--0@xs">
+  <aside class="alert --blue-light f--r@sm col --12@xs --4@sm px--0@xs mt--1@xs">
+    <div class="__header ta--c">Important Actions</div>
     <div class="__body">
       @while(have_rows('lay_actions'))
         @php(the_row())
-        <ul class="list--unstyled">
+        <ul class="fs--sm">
           <li>
             <a class="fw--700" href="{{ get_sub_field('txt_url') }}">{{ get_sub_field('txt_title') }}</a>
             {!! (get_sub_field('rel_action') ? ' <i>('. get_sub_field('rel_action')->name .')</i>' : '') !!}
