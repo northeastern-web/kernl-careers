51c7e2b48752b78a7deea0322b7e560129dad569
Tim Albert <timothy.m.albert@gmail.com>
2017-10-05 09:26:14 -0400

    Change “contacts” to “directory”

--- app/site/PostTypes.php
+++ app/site/PostTypes.php
@@ -6,65 +6,58 @@ class PostTypes
 {
     public function __construct()
     {
-        add_action('init', [$this, 'registerPostTypes']);
-    }
+        add_action('init', function() {
+            register_post_type('article', [
+                'labels'                => [
+                    'name'                => __('Articles'),
+                    'singular_name'       => __('Article'),
+                    'add_new'             => __('Add Article'),
+                    'add_new_item'        => __('Add New Article'),
+                    'edit_item'           => __('Edit Article'),
+                ],
+                'public'                => true,
+                'has_archive'           => true,
+                'rewrite'               => ['slug' => 'article'],
+                'supports'              => ['title', 'excerpt'],
+                'taxonomies'            => [],
+                'hierarchical'          => false,
+                'show_ui'               => true,
+                'show_in_menu'          => true,
+                'menu_position'         => 5,
+                'menu_icon'             => 'dashicons-media-document',
+                'show_in_admin_bar'     => true,
+                'show_in_nav_menus'     => false,
+                'can_export'            => true,
+                'exclude_from_search'   => true,
+                'publicly_queryable'    => true,
+                'capability_type'       => 'post'
+            ]);
 
-    /**
-     * Register custom post types
-     * @return void
-     */
-    public function registerPostTypes()
-    {
-        register_post_type('article', [
-            'labels'                => [
-                'name'                => __('Articles'),
-                'singular_name'       => __('Article'),
-                'add_new'             => __('Add Article'),
-                'add_new_item'        => __('Add New Article'),
-                'edit_item'           => __('Edit Article'),
-            ],
-            'public'                => true,
-            'has_archive'           => true,
-            'rewrite'               => ['slug' => 'article'],
-            'supports'              => ['title', 'excerpt'],
-            'taxonomies'            => [],
-            'hierarchical'          => false,
-            'show_ui'               => true,
-            'show_in_menu'          => true,
-            'menu_position'         => 5,
-            'menu_icon'             => 'dashicons-media-document',
-            'show_in_admin_bar'     => true,
-            'show_in_nav_menus'     => false,
-            'can_export'            => true,
-            'exclude_from_search'   => true,
-            'publicly_queryable'    => true,
-            'capability_type'       => 'post'
-        ]);
-
-        // register_post_type('action', [
-        //     'labels'                => [
-        //         'name'                => __('Actions'),
-        //         'singular_name'       => __('Action'),
-        //         'add_new'             => __('Add Action'),
-        //         'add_new_item'        => __('Add New Action'),
-        //         'edit_item'           => __('Edit Action'),
-        //     ],
-        //     'public'                => true,
-        //     'has_archive'           => true,
-        //     'rewrite'               => ['slug' => 'action'],
-        //     'supports'              => ['title'],
-        //     'taxonomies'            => [],
-        //     'hierarchical'          => false,
-        //     'show_ui'               => true,
-        //     'show_in_menu'          => true,
-        //     'menu_position'         => 5,
-        //     'menu_icon'             => 'dashicons-migrate',
-        //     'show_in_admin_bar'     => true,
-        //     'show_in_nav_menus'     => false,
-        //     'can_export'            => true,
-        //     'exclude_from_search'   => true,
-        //     'publicly_queryable'    => true,
-        //     'capability_type'       => 'post'
-        // ]);
+            register_post_type('directory', [
+                'labels'                => [
+                    'name'                => __('Directory'),
+                    'singular_name'       => __('Directory'),
+                    'add_new'             => __('Add Directory'),
+                    'add_new_item'        => __('Add New Directory'),
+                    'edit_item'           => __('Edit Directory'),
+                ],
+                'public'                => true,
+                'has_archive'           => true,
+                'rewrite'               => ['slug' => 'directory'],
+                'supports'              => ['title'],
+                'taxonomies'            => [],
+                'hierarchical'          => false,
+                'show_ui'               => true,
+                'show_in_menu'          => true,
+                'menu_position'         => 5,
+                'menu_icon'             => 'dashicons-networking',
+                'show_in_admin_bar'     => true,
+                'show_in_nav_menus'     => false,
+                'can_export'            => true,
+                'exclude_from_search'   => true,
+                'publicly_queryable'    => true,
+                'capability_type'       => 'page'
+            ]);
+        });
     }
 }
