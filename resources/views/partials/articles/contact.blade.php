b875fb817695cd4f5c03d301157441dd6a4f6845
Tim Albert <timothy.m.albert@gmail.com>
2017-10-05 11:36:07 -0400

    Update article sidebar

--- resources/views/partials/articles/contact.blade.php
+++ resources/views/partials/articles/contact.blade.php
@@ -1,30 +1,45 @@
 @if(get_sub_field('rel_contact'))
-  <aside class="">
-    <h6 class="fw--700 tt--u">Contact</h6>
-    @foreach (get_sub_field('rel_contact') as $contact)
-        <ul class="list--unstyled fs--sm">
-          <li class="fw--700">{{ $contact->post_title }}</li>
-          @if(get_field('txt_email', $contact->ID))
-            <li>{{ get_field('txt_email', $contact->ID) }}</li>
-          @endif
-          @if(get_field('txt_phone', $contact->ID))
-            <li>{{ get_field('txt_phone', $contact->ID) }}</li>
-          @endif
-          @if(get_field('txt_fax', $contact->ID))
-            <li>{{ get_field('txt_fax', $contact->ID) }}</li>
-          @endif
-          @if(get_field('txt_address', $contact->ID))
-            <li>{!! get_field('txt_address', $contact->ID) !!}</li>
-          @endif
-          @if(get_field('txt_location', $contact->ID))
-            <li>{!! get_field('txt_location', $contact->ID) !!}</li>
-          @endif
-          @if(get_field('txt_hours', $contact->ID))
-            <li>{!! get_field('txt_hours', $contact->ID) !!}</li>
-          @endif
-        </ul>
-      </li>
-    @endforeach
+  <aside class="card --extend">
+    <div class="__header bg--gray-100">
+      <div class="__column">Contact</div>
+    </div>
+
+    <div class="__body pb--1@xs">
+      @foreach (get_sub_field('rel_contact') as $contact)
+        <div class="__title">{{ $contact->post_title }}</div>
+        <div class="__excerpt">
+          <ul class="list--unstyled fs--sm">
+            @if(get_field('txt_email', $contact->ID))
+              <li><a href="mailto:{{ get_field('txt_email', $contact->ID) }}">{{ get_field('txt_email', $contact->ID) }}</a></li>
+            @endif
+            @if(get_field('txt_phone', $contact->ID))
+              <li><a href="tel:{{ get_field('txt_phone', $contact->ID) }}">{{ get_field('txt_phone', $contact->ID) }}</a></li>
+            @endif
+            @if(get_field('txt_fax', $contact->ID))
+              <li><a href="tel:{{ get_field('txt_fax', $contact->ID) }}">{{ get_field('txt_fax', $contact->ID) }}</a></li>
+            @endif
+            @if(get_field('txt_address', $contact->ID))
+              <li>
+                <h6 class="fw--700 mb--0@xs">Mailing Address</h6>
+                {!! get_field('txt_address', $contact->ID) !!}
+              </li>
+            @endif
+            @if(get_field('txt_location', $contact->ID))
+              <li>
+                <h6 class="fw--700 mb--0@xs">Location</h6>
+                {!! get_field('txt_location', $contact->ID) !!}
+              </li>
+            @endif
+            @if(get_field('txt_hours', $contact->ID))
+              <li>
+                <h6 class="fw--700 mb--0@xs">Office Hours</h6>
+                {!! get_field('txt_hours', $contact->ID) !!}
+              </li>
+            @endif
+          </ul>
+        </div>
+      @endforeach
+    </div>
     @php(wp_reset_postdata())
   </aside>
 @endif
--- resources/views/partials/articles/related.blade.php
+++ resources/views/partials/articles/related.blade.php
@@ -1,18 +1,10 @@
 @if(get_field('rel_related'))
-  <article class="card --scroll +list-group">
-    <header class="__header bg--gray-200">
-      <div class="__column">
-        <div>Related Articles</div>
-      </div>
-    </header>
-    <div class="__body">
-      <div class="__excerpt">
-        <ul class="list-group">
-          @foreach (get_field('rel_related') as $article)
-            <li><a href="{{ get_permalink($article->ID) }}">{{ $article->post_title }}</a></li>
-          @endforeach
-        </ul>
-      </div>
-    </div>
-  </article>
+<aside>
+  <h5>Related Articles</h5>
+  <ul class="list-group">
+    @foreach (get_field('rel_related') as $article)
+      <li><a href="{{ get_permalink($article->ID) }}">{{ $article->post_title }}</a></li>
+    @endforeach
+  </ul>
+</aside>
 @endif
