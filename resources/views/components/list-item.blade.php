4bb20b49b5c0b294d60114a4fc6da8c92735f868
Tim Albert <timothy.m.albert@gmail.com>
2017-10-05 17:29:32 -0400

    misc

--- resources/views/components/list-item.blade.php
+++ resources/views/components/list-item.blade.php
@@ -1,6 +1,6 @@
-<article class="__item">
-  <a href="{{ the_permalink() }}">
-    <b>{{ the_title() }}</b><br>
-    {{ get_the_excerpt() }}
-  </a>
-</article>
+<a class="__item" href="{{ the_permalink() }}">
+  <h6 class="mb--0@xs">{{ the_title() }}</h6>
+  <div class="text--gray-600 fs--xs">
+    {{ (get_the_excerpt() ? get_the_excerpt() : 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.') }}
+  </div>
+</a>
