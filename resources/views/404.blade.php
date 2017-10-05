570cab62a855fc1f9cfd7643f571731284244def
Tim Albert <timothy.m.albert@gmail.com>
2017-10-05 09:36:18 -0400

    Update 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
+    <section class="section --contain">
+      <h1>Page Not Found</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
   @endif
 
   {!! get_the_posts_navigation() !!}
