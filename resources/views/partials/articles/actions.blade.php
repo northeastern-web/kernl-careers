db4e61f685040c80f2de124ca55310c5af2ce554
Tim Albert <timothy.m.albert@gmail.com>
2017-10-06 12:56:19 -0400

    Move action name

--- resources/views/partials/articles/actions.blade.php
+++ resources/views/partials/articles/actions.blade.php
@@ -6,10 +6,10 @@
         @while(have_rows('lay_actions'))
           @php(the_row())
           <li>
-            {!! (get_sub_field('rel_action') ? get_sub_field('rel_action')->name .'<br>' : '') !!}
             <a class="fw--700" href="{!! (get_sub_field('opt_type') == 'File' ? get_sub_field('med_file')['url'] : get_sub_field('txt_url')) !!}">
               {{ get_sub_field('txt_title') }}
             </a>
+            {!! (get_sub_field('rel_action') ? '('. get_sub_field('rel_action')->name .')' : '') !!}
           </li>
         @endwhile
       </ul>
