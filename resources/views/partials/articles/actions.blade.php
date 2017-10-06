38945e2baa5c6ab431498bb2128bd2aa5515db46
Tim Albert <timothy.m.albert@gmail.com>
2017-10-06 09:21:37 -0400

    Hide <br> and add margin to action box

--- resources/views/partials/articles/actions.blade.php
+++ resources/views/partials/articles/actions.blade.php
@@ -1,18 +1,18 @@
 @if(have_rows('lay_actions'))
-  <aside class="alert --action f--r@sm col --12@xs --4@sm px--0@xs mt--1@xs">
+  <aside class="alert --action f--r@sm ml--1@sm col --12@xs --4@sm px--0@xs mt--1@xs">
     <div class="__header ta--c">Important Actions</div>
     <div class="__body">
-      @while(have_rows('lay_actions'))
-        @php(the_row())
-        <ul class="list--unstyled fs--sm">
+      <ul class=" fs--sm">
+        @while(have_rows('lay_actions'))
+          @php(the_row())
           <li>
-            {!! (get_sub_field('rel_action') ? get_sub_field('rel_action')->name : '') !!}<br>
+            {!! (get_sub_field('rel_action') ? get_sub_field('rel_action')->name .'<br>' : '') !!}
             <a class="fw--700" href="{!! (get_sub_field('opt_type') == 'File' ? get_sub_field('med_file')['url'] : get_sub_field('txt_url')) !!}">
               {{ get_sub_field('txt_title') }}
             </a>
           </li>
-        </ul>
-      @endwhile
+        @endwhile
+      </ul>
     </div>
   </aside>
 @endif
