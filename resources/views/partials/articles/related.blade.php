d229d63dd3c806eb9e08242a8fb38a7a9ec4233b
Tim Albert <timothy.m.albert@gmail.com>
2017-10-12 16:00:34 -0400

    Update cards for related info

--- resources/views/partials/articles/related.blade.php
+++ resources/views/partials/articles/related.blade.php
@@ -1,10 +1,16 @@
 @if(get_field('rel_related'))
-<aside>
-  <h6 class="fw--700">Related Articles</h6>
-  <ul class="list-group --sm --striped">
-    @foreach (get_field('rel_related') as $article)
-      <li><a href="{{ get_permalink($article->ID) }}">{{ $article->post_title }}</a></li>
-    @endforeach
-  </ul>
+<aside class="card --outline --extend" style="border-color: #d0d0d0;">
+  <div class="__header fw--700 bg--gray-100">
+    <div class="__column">Related Articles</div>
+  </div>
+  <div class="__body py--0@xs">
+    <div class="__excerpt fs--xs">
+      <div class="list-group --sm">
+        @foreach (get_field('rel_related') as $article)
+          <a href="{{ get_permalink($article->ID) }}">{{ $article->post_title }}</a>
+        @endforeach
+      </div>
+    </div>
+  </div>
 </aside>
 @endif
