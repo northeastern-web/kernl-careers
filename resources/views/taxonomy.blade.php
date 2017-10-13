134b74f4ab4c5890f98be9182694b9b187abdd95
Tim Albert <timothy.m.albert@gmail.com>
2017-10-13 15:34:41 -0400

    Update articles to be hierarchical

--- resources/views/taxonomy.blade.php
+++ resources/views/taxonomy.blade.php
@@ -18,13 +18,16 @@
           @foreach ($term_children as $term_child)
             @php
               $query = new \WP_Query([
+                'orderby' => 'menu_order title',
+                'order' => 'ASC',
+                'posts_per_page' => 5,
                 'tax_query' => [
                   ['taxonomy' => $taxonomy, 'field' => 'term_id', 'terms' => $term_child]
                 ]
               ]);
             @endphp
 
-            <section class="section px--0@xs">
+            <section class="section px--0@xs" id="tax-{{ get_term_by('term_id', $term_child, $taxonomy)->slug }}">
               <header class="__header">
                 <div class="f--r@xs fs--xs pt--1@xs"><a href="{{ get_term_link(get_term_by('term_id', $term_child, $taxonomy)) }}">View More</a></div>
                 <h1 class="__title">{{ get_term_by('term_id', $term_child, $taxonomy)->name }}</h1>
@@ -52,6 +55,22 @@
       </div>
 
       <div class="col --3@xs">
+        @if ($term_children)
+          <aside class="card --outline --extend" style="border-color: #d0d0d0;">
+            <div class="__header fw--700 bg--gray-100">
+              <div class="__column">Navigation</div>
+            </div>
+            <div class="__body py--0@xs">
+              <div class="__excerpt fs--xs">
+                <div class="list-group --sm">
+                  @foreach ($term_children as $term_child)
+                    <a href="#tax-{{ get_term_by('term_id', $term_child, $taxonomy)->slug }}">{{ get_term_by('term_id', $term_child, $taxonomy)->name }}</a>
+                  @endforeach
+                </div>
+              </div>
+            </div>
+          </aside>
+        @endif
       </div>
     </div>
   </div>
