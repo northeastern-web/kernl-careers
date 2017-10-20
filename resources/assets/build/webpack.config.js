a77a0c2e3817e3f7c635bf1bb3d9d0691864b749
rkingston <r.kingston@northeastern.edu>
2017-10-20 16:32:14 -0400

    fonts path, update jquery version

--- resources/assets/build/webpack.config.js
+++ resources/assets/build/webpack.config.js
@@ -108,7 +108,7 @@ let webpackConfig = {
         loader: 'url',
         options: {
           limit: 4096,
-          outputPath: 'vendor/',
+          outputPath: 'fonts/',
           name: `${config.cacheBusting}.[ext]`,
         },
       },
