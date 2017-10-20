e2a1134377daeb1ed936defd95b8f07744c40a4b
Tim Albert <timothy.m.albert@gmail.com>
2017-10-20 09:31:29 -0400

    Minor search updates

--- resources/views/components/modal-search.blade.php
+++ resources/views/components/modal-search.blade.php
@@ -1,14 +1,14 @@
 <div class="modal fade --dark --search" id="modal_search" tabindex="-1" role="dialog" aria-labelledby="modal_search_label" aria-hidden="true">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
   <div class="modal-dialog __dialog" role="document">
     <div class="__content">
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
       <div class="__body">
         <form action="{{ home_url() }}" method="GET">
           <div class="form__group">
             <label for="exampleText" class="sr--only">Search</label>
-            <input name="s" type="search" class="form__control mt--2@xs" id="exampleText" aria-describedby="textHelp" placeholder="Well, lookie here...">
+            <input name="s" type="search" class="form__control mt--2@xs" id="exampleText" aria-describedby="textHelp" placeholder="Search by keyword">
           </div>
         </form>
       </div>
