efcc1d42b2147e9829b4eba87c7628155a95f7ef
Tim Albert <timothy.m.albert@gmail.com>
2017-10-25 16:31:48 -0400

    Drop banner from search results

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<header class="section --banner">
+<header class="section">
   <div class="__header">
     <h1 class="__title">Search Results</h1>
   </div>
+
+  <form action="{{ home_url() }}" method="GET">
+    <div class="form__group">
+      <input name="s" type="text" class="form__control form__control--lg" placeholder="" value="{{ get_search_query() }}">
+    </div>
+  </form>
 </header>
 
 <section class="section">
   <div class="row">
     <div class="--9@sm">
-      <form action="{{ home_url() }}" method="GET">
-        <div class="form__group">
-          <input name="s" type="text" class="form__control form__control--lg" placeholder="" value="{{ get_search_query() }}">
-        </div>
-      </form>
-
       @if (!have_posts())
         <div class="alert --red --outline">
           <div class="__body">
