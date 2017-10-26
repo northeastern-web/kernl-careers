7774084325796f57e25c46d32aa43ea335040b78
Tim Albert <timothy.m.albert@gmail.com>
2017-10-26 17:01:46 -0400

    Other style edits

--- resources/views/registrar/header-article.blade.php
+++ resources/views/registrar/header-article.blade.php
@@ -1,10 +1,12 @@
-<header class="section header">
-  <div class="__pretitle">
-    <nav class="breadcrumb +icon +chevron mb--1@xs">
-      @foreach (\App\Site::getTaxHierarchy(get_the_ID(), 'group') as $term)
-        <a class="text--gray-700" href="{{ get_term_link($term->term_id, 'group') }}">{{ $term->name }}</a>
-      @endforeach
-    </nav>
+<header class="section --header">
+  <div class="__header">
+    <div class="__pretitle">
+      <nav class="breadcrumb +icon +chevron mb--1@xs">
+        @foreach (\App\Site::getTaxHierarchy(get_the_ID(), 'group') as $term)
+          <a class="text--gray-700" href="{{ get_term_link($term->term_id, 'group') }}">{{ $term->name }}</a>
+        @endforeach
+      </nav>
+    </div>
+    <h1 class="__title">{{ the_title() }}</h1>
   </div>
-  <h1 class="__title">{{ the_title() }}</h1>
 </header>
