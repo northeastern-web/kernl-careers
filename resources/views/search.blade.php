e90ed1b4d8a012582d83115cdff8733a24d86795
Tim Albert <timothy.m.albert@gmail.com>
2017-10-26 17:01:29 -0400

    Update new searches 👌

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,21 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<header class="section">
-  <div class="__header">
-    <h1 class="__title">Search Results</h1>
-  </div>
-
-  <form action="{{ home_url() }}" method="GET">
-    <div class="form__group">
-      <input name="s" type="text" class="form__control form__control--lg" placeholder="" value="{{ get_search_query() }}">
-    </div>
-  </form>
-</header>
-
 <section class="section">
+  <div class="__header">
+    <h1 class="__title mb--1@xs">Search Results</h1>
+  </div>
   <div class="row">
-    <div class="--9@sm">
+    <div class="col --8@sm --12@xs">
+      <form action="{{ home_url() }}" method="GET" class="mb--2@xs">
+        <div class="__group __search +line">
+          <input name="s" type="text" class="__control" placeholder="Search by keyword" value="{{ get_search_query() }}">
+          <button type="submit" class="btn btn--primary">Go</button>
+        </div>
+        <small class="fs--xs text--gray-500"><b>Returning:</b> XYZ results</small>
+      </form>
+
       @if (!have_posts())
         <div class="alert --red --outline">
           <div class="__body">
