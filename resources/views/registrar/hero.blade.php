323c753911a449013d68a2e519e898ff35f60c6f
rkingston <r.kingston@northeastern.edu>
2017-11-02 13:21:46 -0400

    add hero content (static) and style

--- resources/views/registrar/hero.blade.php
+++ resources/views/registrar/hero.blade.php
@@ -1,3 +1,46 @@
 <?php /** (Hero) */ ?>
 
-<p class="fs--lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+<div class="row">
+  <div class="col --12@xs --6@md --offset-3@md">
+    <form action="" method="GET">
+      <div class="__group __search">
+        <input class="__control" name="s" type="text" placeholder="Search" />
+        <button class="btn btn--primary" type="submit">Go</button>
+      </div>
+    </form>
+  </div>
+</div>
+<div class="row">
+  <div class="col --12@xs --4@md --offset-4@md">
+    <h3 class="ta--c tt--caps fs--root --popular">Popular Searches</h3>
+  </div>
+</div>
+<div class="row">
+  <div class="col --12@xs --4@md --offset-2@md --3@lg --offset-3@lg">
+    <ul class="--popular">
+      <li>
+        <a href="{{ home_url() }}/article/2017-2018-academic-catalog/">2017-2018 Catalog &amp; Course Descriptions</a>
+      </li>
+      <li>
+        <a href="{{ home_url() }}/article/combined-majors/">Combined Majors</a>
+      </li>
+      <li>
+        <a href="{{ home_url() }}/article/university-withdrawal/">University Withdrawal</a>
+      </li>
+    </ul>
+    </div>
+    <div class="col --12@xs --4@md --3@lg">
+      <ul class="--popular">
+        <li>
+          <a href="{{ home_url() }}/article/add-a-class/">Add a Class</a>
+        </li>
+        <li>
+          <a href="{{ home_url() }}/article/petition-to-elect-passfail-grade/">Petition to Elect Pass/Fail Grade</a>
+        </li>
+        <li>
+          <a href="{{ home_url() }}/article/request-a-room/">Request a Room</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
