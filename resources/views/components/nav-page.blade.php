09a37c1ae80679d16eeaa4c25d75269a046ff157
Tim Albert <timothy.m.albert@gmail.com>
2017-11-02 11:20:03 -0400

    Update for kernl

--- resources/views/components/nav-page.blade.php
+++ resources/views/components/nav-page.blade.php
@@ -1,11 +1,11 @@
-<nav class="nav">
+<nav class="nav --interior">
   <div class="hidden--up@md ta--c p--b-1@xs o--t-1@xs">
-    <button class="nav__toggler btn btn--outline btn--sm btn--primary">
+    <button class="__toggler btn btn--outline btn--sm btn--primary">
       <i class="ion-android-funnel"></i> &nbsp;Page Menu
     </button>
   </div>
 
-  <ul class="nav__list">
-    <?= \PODS\Doric\Navigation::display(); ?>
+  <ul class="__list">
+    <?= \Kernl\Navigation::display(); ?>
   </ul>
 </nav>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -6,14 +6,16 @@
     @include('layouts.chrome.header-singular')
 
     @if (\Kernl\Navigation::display() && !get_field('bool_horizontal_nav'))
-      <div class="row">
-        <div class="col --12@xs --3@lg">
-          @include('components.nav-page')
+      <section class="section">
+        <div class="row">
+          <div class="col --12@xs --3@lg">
+            @include('components.nav-page')
+          </div>
+          <div class="col --12@xs --9@lg">
+            @include('layouts.sections.section')
+          </div>
         </div>
-        <div class="col --12@xs --9@lg">
-          @include('layouts.sections.section')
-        </div>
-      </div>
+      </section>
     @else
       @include('layouts.sections.section')
     @endif
