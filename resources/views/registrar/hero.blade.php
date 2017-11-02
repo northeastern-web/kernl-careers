e4d31fb92070c952756b4c5ae22153493ee98494
rkingston <r.kingston@northeastern.edu>
2017-11-02 13:48:24 -0400

    adjust hero media queries

--- resources/views/registrar/hero.blade.php
+++ resources/views/registrar/hero.blade.php
@@ -1,7 +1,7 @@
 <?php /** (Hero) */ ?>
 
 <div class="row">
-  <div class="col --12@xs --6@md --offset-3@md">
+  <div class="col --12@xs --10@sm --offset-1@sm --8@md --offset-2@md --6@lg --offset-3@lg">
     <form action="" method="GET">
       <div class="__group __search">
         <input class="__control" name="s" type="text" placeholder="Search" />
@@ -11,12 +11,12 @@
   </div>
 </div>
 <div class="row">
-  <div class="col --12@xs --4@md --offset-4@md">
+  <div class="col --12@xs --10@sm --offset-1@sm --6@md --offset-3@md --4@lg --offset-4@lg">
     <h3 class="ta--c tt--caps fs--root --popular">Popular Searches</h3>
   </div>
 </div>
 <div class="row">
-  <div class="col --12@xs --4@md --offset-2@md --3@lg --offset-3@lg">
+  <div class="col --12@xs --10@sm --offset-1@sm --5@md --4@lg --offset-2@lg --3@xl --offset-3@xl">
     <ul class="--popular">
       <li>
         <a href="{{ home_url() }}/article/2017-2018-academic-catalog/">2017-2018 Catalog &amp; Course Descriptions</a>
@@ -29,7 +29,7 @@
       </li>
     </ul>
     </div>
-    <div class="col --12@xs --4@md --3@lg">
+    <div class="col --12@xs --10@sm --offset-1@sm --5@md --offset-0@md --4@lg --3@lg">
       <ul class="--popular">
         <li>
           <a href="{{ home_url() }}/article/add-a-class/">Add a Class</a>
