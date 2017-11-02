838fda0964af06ebcb21ce4283548eb6e0b8598d
rkingston <r.kingston@northeastern.edu>
2017-11-02 16:31:33 -0400

    add ie condition; add sticky

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,11 @@
   @include('layouts.chrome.head')
   <body>
     <a class="skip alert" href="#main_content">Skip to main content</a>
+    <!--[if IE]>
+      <div class="alert bg--yellow-light fs--sm pa--1@xs pa--2@md">
+        <?php _e('<b><i>Note</i></b>: You are using an <strong>outdated</strong> browser. Please <a class="text--red" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+      </div>
+    <![endif]-->
     {!! \Kernl\Utility::getTagManager(\WP_ENV) !!}
     @php(do_action('get_header'))
     @include('layouts.chrome.masthead')
