3ab1e9fbe58bb0eb117ca55e5085c55d063ec31c
rkingston <r.kingston@northeastern.edu>
2017-11-10 10:30:08 -0500

    update homepage layout, style, colors

---
+++ resources/views/registrar/prefooter.blade.php
@@ -0,0 +1,29 @@
+<?php /** (Prefooter) */ ?>
+
+<h3 class="fs--root tt--caps ta--c mb--1@xs mb--2@md --popular">Helpful Topics</h3>
+<div class="row ta--c">
+  <div class="col --6@xs --3@md --2@lg mb--1@xs mb--0@md --offset-2@lg">
+    <a class="" href="#">
+      <i class="__icon --lg" data-feather="copy"></i><br>
+      Transcripts &amp; Verifications
+    </a>  
+  </div>
+  <div class="col --6@xs --3@md --2@lg mb--1@xs mb--0@md">
+    <a class="" href="#">
+      <i class="__icon --lg" data-feather="layers"></i><br>
+      Catalogs &amp; Course Descriptions
+    </a>  
+  </div>
+  <div class="col --6@xs --3@md --2@lg mb--1@xs mb--0@md">
+    <a class="" href="#">
+      <i class="__icon --lg" data-feather="book"></i><br>
+      Registration &amp; Classes
+    </a>  
+  </div>
+  <div class="col --6@xs --3@md --2@lg mb--1@xs mb--0@md">
+    <a class="" href="#">
+      <i class="__icon --lg" data-feather="calendar"></i><br>
+      Academic Calendars
+    </a>  
+  </div>
+</div>
