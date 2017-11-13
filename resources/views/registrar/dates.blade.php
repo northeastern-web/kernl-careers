6f71ba65188b00c35ebab4d1cef92c0e6aab22e0
rkingston <r.kingston@northeastern.edu>
2017-11-13 14:32:06 -0500

    update styles and spacing and more

--- resources/views/registrar/dates.blade.php
+++ resources/views/registrar/dates.blade.php
@@ -1,61 +1,85 @@
 <?php /** (Dates) */ ?>
 
-<h3 class="fs--root tt--caps ta--c mb--1@xs mb--2@md --popular">Important Dates</h3>
+<h3 class="fs--root tt--caps ta--c mb--1@xs mb--3@md --popular">Important Dates</h3>
 <div class="row">
-  <div class="col --3@md --12@xs">
-    <div class="__copy">
-      <article class="card --tile --outline +scroll">
-        <div class="badge --red tt--caps fw--700">Mon, Nov 13</div>
-        <div class="__body">
+  <div class="col --2@md --12@xs">
+    <div class="__copy +equal">
+      <article class="card --tile +noshadow">
+        <div class="__date fs--xs tt--caps fw--700">Mon, Nov 13</div>
+        <div class="__body pl--0@md pt--0@md">
           <div class="__excerpt">
-            <p class="fs--sm mb--0@xs">UG:</p>
             <h2 class="__title">First day of spring class registration for continuing students</h2>
+            <p class="fs--sm">Undergraduate</p>
           </div>
+        </div>
+      </article>
+    </div>
+  </div>
+  <div class="col --2@md --12@xs">
+    <div class="__copy +equal">
+      <article class="card --tile +noshadow">
+        <div class="__date fs--xs tt--caps fw--700">Mon, Nov 13</div>
+        <div class="__body pl--0@md pt--0@md">
           <div class="__excerpt">
-            <p class="fs--sm mb--0@xs">CPSU:</p>
             <h2 class="__title">Last day to drop a second-half fall session class without a W grade</h2>
+            <p class="fs--sm">CPS Undergraduate</p>
           </div>
         </div>
       </article>
     </div>
   </div>
 
-  <div class="col --3@md --12@xs">
-    <div class="__copy">
-      <article class="card --tile --outline">
-        <div class="badge --red tt--caps fw--700">Sun, Nov 19</div>
-        <div class="__body">
+  <div class="col --2@md --12@xs">
+    <div class="__copy +equal">
+      <article class="card --tile +noshadow">
+        <div class="__date fs--xs tt--caps fw--700">Sun, Nov 19</div>
+        <div class="__body pl--0@md pt--0@md">
           <div class="__excerpt">
-            <p class="fs--sm mb--0@xs">CPSG:</p>
             <h2 class="__title">First day of Thanksgiving recess (no classes)</h2>
+            <p class="fs--sm">CPS Graduate</p>
           </div>
         </div>
       </article>
     </div>
   </div>
 
-  <div class="col --3@md --12@xs">
-    <div class="__copy">
-      <article class="card --tile --outline">
-        <div class="badge --red tt--caps fw--700"><b>Sun, Nov 22</b></div>
-        <div class="__body">
+  <div class="col --2@md --12@xs">
+    <div class="__copy +equal">
+      <article class="card --tile +noshadow">
+        <div class="__date fs--xs tt--caps fw--700"><b>Sun, Nov 22</b></div>
+        <div class="__body pl--0@md pt--0@md">
           <div class="__excerpt">
-            <p class="fs--sm mb--0@xs">UG, G, CPSG:</p>
             <h2 class="__title">First day of Thanksgiving recess (no classes)</h2>
+            <p class="fs--sm">Undergraduate, Graduate, CPS Graduate</p>
           </div>
         </div>
       </article>
     </div>
   </div>
 
-  <div class="col --3@md --12@xs">
-    <div class="__copy">
-      <article class="card --tile --outline">
-        <div class="badge --red tt--caps fw--700">Mon, Nov 27</div>
-        <div class="__body">
+  <div class="col --2@md --12@xs">
+    <div class="__copy +equal">
+      <article class="card --tile +noshadow">
+        <div class="__date fs--xs tt--caps fw--700">Mon, Nov 27</div>
+        <div class="__body pl--0@md pt--0@md">
           <div class="__excerpt">
-            <p class="fs--sm mb--0@xs">All Students:</p>
             <h2 class="__title">Classes Resume</h2>
+            <p class="fs--sm">All Students</p>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+
+  <div class="col --2@md --12@xs">
+    <div class="__copy +equal">
+      <article class="card --tile +noshadow">
+        <div class="__date fs--xs tt--caps fw--700">Mon, Nov 27</div>
+        <div class="__body pl--0@md pt--0@md">
+          <div class="__excerpt">
+            <h2 class="__title">Classes Resume</h2>
+            <p class="fs--sm">All Students</p>
+
           </div>
         </div>
       </article>
@@ -64,8 +88,8 @@
 
   <div class="col --12@xs" data-col="5" data-acf="opt_column">
     <div class="__copy">
-      <p class="ta--c fs--sm mb--0@xs">
-        <a class="btn --sm --outline --red" href="//localhost:3000/article/academic-year-2017-2018-current/">See All Dates</a>
+      <p class="ta--c fs--sm">
+        <a class="btn --sm" href="//localhost:3000/article/academic-year-2017-2018-current/">See All Dates</a>
       </p>
     </div>
   </div>
