79d7bd714576ad22fb71246afcfe0174889bcae9
rkingston <r.kingston@northeastern.edu>
2017-11-15 11:01:20 -0500

    update homepage urls

--- resources/views/registrar/services.blade.php
+++ resources/views/registrar/services.blade.php
@@ -6,19 +6,19 @@
     <div class="__copy">
       <h2><a href="#"><i class="__icon" data-feather="copy"></i>Academic Catalog</a></h2>
       <div class="list-group +indent mb--4@md">
-        <a class="__item" href="/article/2017-2018-academic-catalog/">
+        <a class="__item" href="<?= home_url(); ?>/article/2017-2018-academic-catalog/">
           <h6 class="mb--0@xs">2017-2018 Catalog &amp; Course Descriptions</h6>
           <div class="text--gray-600 fs--xs">
             The Academic Catalog is a comprehensive publication of Northeastern's course catalog and course descriptions.
           </div>
         </a>
-        <a class="__item" href="/group/catalog-information/">
+        <a class="__item" href="<?= home_url(); ?>/group/catalog-information/">
           <h6 class="mb--0@xs">Catalog Information</h6>
           <div class="text--gray-600 fs--xs">
             NU Path, Course Description Requests and more.
           </div>
         </a>
-        <a class="__item" href="/article/combined-majors/">
+        <a class="__item" href="<?= home_url(); ?>/article/combined-majors/">
           <h6 class="mb--0@xs">Combined Majors</h6>
           <div class="text--gray-600 fs--xs">
             Students with academic interests not available in the context of existing majors, minors, and&nbsp;double majors may propose a combined or independent major. 
@@ -31,19 +31,19 @@
     <div class="__copy">
       <h2><a href="#"><i class="__icon" data-feather="layers"></i> Registration &amp; Classes</a></h2>
       <div class="list-group +indent mb--4@md">
-        <a class="__item" href="/article/registration-dates/">
+        <a class="__item" href="<?= home_url(); ?>/article/registration-dates/">
           <h6 class="mb--0@xs">Registration Dates and Deadlines</h6>
           <div class="text--gray-600 fs--xs">
             Important registration dates and deadlines for all student levels, including class drop and add deadlines.
           </div>
         </a>
-        <a class="__item" href="/article/add-a-class/">
+        <a class="__item" href="<?= home_url(); ?>/article/add-a-class/">
           <h6 class="mb--0@xs">Add a Class</h6>
           <div class="text--gray-600 fs--xs">
             Register for classes at Northeastern.
           </div>
         </a>
-        <a class="__item" href="/article/class-changes/">
+        <a class="__item" href="<?= home_url(); ?>/article/class-changes/">
           <h6 class="mb--0@xs">Class Changes</h6>
           <div class="text--gray-600 fs--xs">
             Request to make a change to a class section.
@@ -56,19 +56,19 @@
     <div class="__copy">
       <h2><a href="#"><i class="__icon" data-feather="book"></i>Academic Records</a></h2>
       <div class="list-group +indent">
-        <a class="__item" href="/article/transcript-requests/">
+        <a class="__item" href="<?= home_url(); ?>/article/transcript-requests/">
           <h6 class="mb--0@xs">Transcript Requests</h6>
           <div class="text--gray-600 fs--xs">
             Transcript processing time is usually three to five business days and will not be processed for students with financial holds.
           </div>
         </a>
-        <a class="__item" href="/article/student-verification-requests/">
+        <a class="__item" href="<?= home_url(); ?>/article/student-verification-requests/">
           <h6 class="mb--0@xs">Current/Former Student Verification Requests</h6>
           <div class="text--gray-600 fs--xs">
             Verify that a student is currently enrolled in the University, and is registered into classes for the starting term.
           </div>
         </a>
-        <a class="__item" href="/article/how-to-calculate-your-gpa/">
+        <a class="__item" href="<?= home_url(); ?>/article/how-to-calculate-your-gpa/">
           <h6 class="mb--0@xs">How to Calculate Your GPA</h6>
           <div class="text--gray-600 fs--xs">
             Instructions for students to manually calculate their grade-point average (GPA).
@@ -81,19 +81,19 @@
     <div class="__copy">
       <h2><a href="#"><i class="__icon" data-feather="calendar"></i> Faculty &amp; Staff</a></h2>
       <div class="list-group +indent">
-        <a class="__item" href="/article/fact/">
+        <a class="__item" href="<?= home_url(); ?>/article/fact/">
           <h6 class="mb--0@xs">Faculty-Advisor Communication Tool (FACT)</h6>
           <div class="text--gray-600 fs--xs">
             The Faculty-Advisor Communication Tool (FACT) has many intertwining goals. 
           </div>
         </a>
-        <a class="__item" href="/article/requesting-student-data/">
+        <a class="__item" href="<?= home_url(); ?>/article/requesting-student-data/">
           <h6 class="mb--0@xs">Requesting Student Data</h6>
           <div class="text--gray-600 fs--xs">
             The Office of the University Registrar provides data to faculty and staff primarily for internal education needs (student/course/program/department level)
           </div>
         </a>
-        <a class="__item" href="/article/faculty-grade-submission/">
+        <a class="__item" href="<?= home_url(); ?>/article/faculty-grade-submission/">
           <h6 class="mb--0@xs">Faculty Grade Submission</h6>
           <div class="text--gray-600 fs--xs">
             Faculty grade submission deadlines and frequently asked questions.
