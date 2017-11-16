f2694c0cb3da47d45e919e3353d4fc6b4c0296c8
Tim Albert <timothy.m.albert@gmail.com>
2017-11-16 16:55:11 -0500

    Change services to list articles

---
+++ resources/views/registrar/list-articles.blade.php
@@ -0,0 +1,31 @@
+<?php /** (List Articles) */ ?>
+
+@php
+  global $query_string;
+  $taxonomy = (isset($query_string['taxonomy']) ? $query_string['taxonomy'] : 'group');
+  $term = (isset($query_string['term']) ? $query_string['term'] : 'catalog');
+  $count = (isset($query_string['count']) ? $query_string['count'] : 3);
+  $q = new \WP_Query([
+    'post_type' => 'article',
+    'orderby' => 'menu_order title',
+    'order' => 'ASC',
+    'posts_per_page' => $count,
+    'tax_query' => [
+      ['taxonomy' => $taxonomy, 'field' => 'slug', 'terms' => $term]
+    ]
+  ]);
+@endphp
+
+@if ($q->have_posts())
+  <section class="mb--3@xs">
+    @include('registrar.list-header')
+
+    <div class="list-group +indent">
+      @while ($q->have_posts())
+        @php($q->the_post())
+        @include('registrar.list-item')
+      @endwhile
+      @php(wp_reset_postdata())
+    </div>
+  </section>
+@endif
