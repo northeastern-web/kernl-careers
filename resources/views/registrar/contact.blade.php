01ffef7598b174e6047080cd4b11dee3547f91cf
Tim Albert <timothy.m.albert@gmail.com>
2017-11-20 16:36:52 -0500

    Add custom title to contacts

--- resources/views/registrar/contact.blade.php
+++ resources/views/registrar/contact.blade.php
@@ -9,16 +9,16 @@
         <div class="__excerpt fs--sm">
           <ul class="list--unstyled">
             <li class="mb--1@xs">
-              <h6 class="fw--700 mb--0@xs">{{ $contact->post_title }}</h6>
+              <h6 class="fw--700 mb--0@xs">{{ get_field('txt_title', $contact->ID) }}</h6>
               <ul class="list--unstyled mx--0@xs">
                 @if(get_field('txt_email', $contact->ID))
                   <li><a href="mailto:{{ get_field('txt_email', $contact->ID) }}">{{ get_field('txt_email', $contact->ID) }}</a></li>
                 @endif
                 @if(get_field('txt_phone', $contact->ID))
-                  <li>tel: <a href="tel:{{ get_field('txt_phone', $contact->ID) }}">{{ get_field('txt_phone', $contact->ID) }}</a></li>
+                  <li><a href="tel:{{ get_field('txt_phone', $contact->ID) }}">{{ get_field('txt_phone', $contact->ID) }}</a></li>
                 @endif
                 @if(get_field('txt_fax', $contact->ID))
-                  <li>fax: <a href="tel:{{ get_field('txt_fax', $contact->ID) }}">{{ get_field('txt_fax', $contact->ID) }}</a></li>
+                  <li><a href="tel:{{ get_field('txt_fax', $contact->ID) }}">{{ get_field('txt_fax', $contact->ID) }}</a> (fax)</li>
                 @endif
               </ul>
             </li>
