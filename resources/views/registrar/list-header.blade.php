8af9dc3a7ad7b240a3c3c33e9c3d0e15fbb2470e
Tim Albert <timothy.m.albert@gmail.com>
2017-11-20 15:08:32 -0500

    Update to term_id

--- resources/views/registrar/list-header.blade.php
+++ resources/views/registrar/list-header.blade.php
@@ -1,15 +1,15 @@
 <header class="__header --archive mb--1@xs">
-  <h2 class="__title{{ get_field('txt_icon', get_term_by('slug', $term, $taxonomy)) ? ' +icon' : '' }}">
-    <a class="__link mr--1@xs" href="{{ get_term_link(get_term_by('slug', $term, $taxonomy)) }}">
-      @if(get_field('txt_icon', get_term_by('slug', $term, $taxonomy)))
-        <i class="__icon --thin text--red" data-feather="{{ get_field('txt_icon', get_term_by('slug', $term, $taxonomy)) }}"></i>
+  <h2 class="__title{{ get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)) ? ' +icon' : '' }}">
+    <a class="__link mr--1@xs" href="{{ get_term_link(get_term_by('term_id', $term, $taxonomy)) }}">
+      @if(get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)))
+        <i class="__icon --thin text--red" data-feather="{{ get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)) }}"></i>
       @endif
 
-      {{ get_term_by('slug', $term, $taxonomy)->name }}
+      {{ get_term_by('term_id', $term, $taxonomy)->name }}
     </a>
 
     @if($q->found_posts > $count)
-      <a class="btn --gray-500 --outline --xs" href="{{ get_term_link(get_term_by('slug', $term, $taxonomy)) }}">{{ $q->found_posts }} Articles</a>
+      <a class="btn --gray-500 --outline --xs" href="{{ get_term_link(get_term_by('term_id', $term, $taxonomy)) }}">{{ $q->found_posts }} Articles</a>
     @endif
   </h2>
 </header>
