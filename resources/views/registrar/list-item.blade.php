f6f5b8bf2077e0ff42374ad79aca14b22385a4bb
Tim Albert <timothy.m.albert@gmail.com>
2017-11-27 15:02:52 -0500

    Allow html in excerpt

--- resources/views/registrar/list-item.blade.php
+++ resources/views/registrar/list-item.blade.php
@@ -1,15 +1,11 @@
 <a class="__item" href="{{ the_permalink() }}">
-  @if (has_term('form','type'))
-    <h6 class="mb--0@xs +form">
-  @else
-    <h6 class="mb--0@xs">
-  @endif
-    {{ the_title() }}
+  <h6 class="mb--0@xs {{ (has_term('form','type') ? '+form' : '') }}">
+    {!! the_title() !!}
     @if (has_term('form','type'))
       <i class="text--gray-300 +form" data-feather="file-text"></i>
     @endif
   </h6>
   <div class="__excerpt text--gray-600 fs--xs pr--1@xs">
-    {{ (get_the_excerpt() ? get_the_excerpt() : '!! Still needs excerpt !!') }}
+    {!! (get_the_excerpt() ? get_the_excerpt() : '!! Still needs excerpt !!') !!}
   </div>
 </a>
