c32afc5d17d3ba1c2d9f2ddb0b903dcd13c0e02b
Tim Albert <timothy.m.albert@gmail.com>
2017-11-28 16:23:47 -0500

    Add condition layer for vars

--- resources/views/registrar/list-header.blade.php
+++ resources/views/registrar/list-header.blade.php
@@ -1,11 +1,11 @@
 <header class="__header --archive">
   <h2 class="__title {{ ($icon || get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)) ? '+icon' : '') }}">
     <a class="__link mr--1@xs" href="{{ get_term_link(get_term_by('term_id', $term, $taxonomy)) }}">
-      @if($icon || get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)))
-        <i class="__icon --thin text--red" data-feather="{{ ($icon ? $icon : get_field('txt_icon', get_term_by('term_id', $term, $taxonomy))) }}"></i>
+      @if(isset($icon) || get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)))
+        <i class="__icon --thin text--red" data-feather="{{ (isset($icon) ? $icon : get_field('txt_icon', get_term_by('term_id', $term, $taxonomy))) }}"></i>
       @endif
 
-      {{ ($title ? $title : get_term_by('term_id', $term, $taxonomy)->name) }}
+      {{ (isset($title) ? $title : get_term_by('term_id', $term, $taxonomy)->name) }}
     </a>
 
     @if($q->found_posts > $count)
