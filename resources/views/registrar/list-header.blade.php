942b452f65e70fa19e4e3d2515bc748fc8e5a58a
Tim Albert <timothy.m.albert@gmail.com>
2017-11-29 11:24:30 -0500

    Add isset condition to icon

--- resources/views/registrar/list-header.blade.php
+++ resources/views/registrar/list-header.blade.php
@@ -1,5 +1,5 @@
 <header class="__header --archive">
-  <h2 class="__title {{ ($icon || get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)) ? '+icon' : '') }}">
+  <h2 class="__title {{ (isset($icon) || get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)) ? '+icon' : '') }}">
     <a class="__link mr--1@xs" href="{{ get_term_link(get_term_by('term_id', $term, $taxonomy)) }}">
       @if(isset($icon) || get_field('txt_icon', get_term_by('term_id', $term, $taxonomy)))
         <i class="__icon --thin text--red" data-feather="{{ (isset($icon) ? $icon : get_field('txt_icon', get_term_by('term_id', $term, $taxonomy))) }}"></i>
