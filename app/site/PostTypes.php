e9f19524508ce3a61acde26050c14a543538ee39
Tim Albert <timothy.m.albert@gmail.com>
2017-12-13 10:35:51 -0500

    Clean up registrar

--- app/site/PostTypes.php
+++ app/site/PostTypes.php
@@ -6,32 +6,38 @@ class PostTypes
 {
     public function __construct()
     {
+        $this->register();
+    }
+
+
+    protected function register()
+    {
         add_action('init', function() {
-            // register_post_type('article', [
-            //     'labels'                => [
-            //         'name'                => __('Articles'),
-            //         'singular_name'       => __('Article'),
-            //         'add_new'             => __('Add Article'),
-            //         'add_new_item'        => __('Add New Article'),
-            //         'edit_item'           => __('Edit Article'),
-            //     ],
-            //     'public'                => true,
-            //     'has_archive'           => true,
-            //     'rewrite'               => ['slug' => 'article'],
-            //     'supports'              => ['title', 'excerpt'],
-            //     'taxonomies'            => ['post_tag'],
-            //     'hierarchical'          => true,
-            //     'show_ui'               => true,
-            //     'show_in_menu'          => true,
-            //     'menu_position'         => 5,
-            //     'menu_icon'             => 'dashicons-media-document',
-            //     'show_in_admin_bar'     => true,
-            //     'show_in_nav_menus'     => true,
-            //     'can_export'            => true,
-            //     'exclude_from_search'   => false,
-            //     'publicly_queryable'    => true,
-            //     'capability_type'       => 'post'
-            // ]);
+            register_post_type('example', [
+                'labels'                => [
+                    'name'                => __('Examples'),
+                    'singular_name'       => __('Example'),
+                    'add_new'             => __('Add Example'),
+                    'add_new_item'        => __('Add New Example'),
+                    'edit_item'           => __('Edit Example'),
+                ],
+                'public'                => true,
+                'has_archive'           => true,
+                'rewrite'               => ['slug' => 'example'],
+                'supports'              => ['title', 'excerpt'],
+                'taxonomies'            => ['post_tag'],
+                'hierarchical'          => true,
+                'show_ui'               => true,
+                'show_in_menu'          => true,
+                'menu_position'         => 5,
+                'menu_icon'             => 'dashicons-media-document',
+                'show_in_admin_bar'     => true,
+                'show_in_nav_menus'     => true,
+                'can_export'            => true,
+                'exclude_from_search'   => false,
+                'publicly_queryable'    => true,
+                'capability_type'       => 'post'
+            ]);
         });
     }
 }
--- resources/views/layouts/chrome/masthead.blade.php
+++ resources/views/layouts/chrome/masthead.blade.php
@@ -1,5 +1,4 @@
-<header
-  class="masthead {{ (get_field('bool_megamenu','option') ? '--megamenu' : '') }} {{ (\App\Site::getMenu('Utility Navigation') ? '+utility' : '') }} {{ (get_field('bool_masthead_overylay','option') ? '--overlay' : '') }}">
+<header class="masthead {{ (get_field('bool_megamenu','option') ? '--megamenu' : '') }} {{ (\Kernl\Masthead::getMenu('Utility Navigation') ? '+utility' : '') }} {{ (get_field('bool_masthead_overylay','option') ? '--overlay' : '') }}">
   <a class="__logo" href="{{ home_url('/') }}" data-ga-click="Home Logo">
     <img class="__logo__image" src="<?= get_field('med_logo','option'); ?>" alt="<?= get_bloginfo('name', 'display'); ?> logo">
   </a>
@@ -23,22 +22,12 @@
         'walker' => new \Kernl\Masthead()
       ]);
       @endphp
-
-    {{--
-      <ul class="__list">
-        @foreach (\App\Site::getMenu('Primary Navigation') as $item)
-          <li class="__item {{ (\App\Site::isActiveMenu('Primary Navigation', $item) ? '--active' : '') }}">
-            <a class="__link" href="{{ $item->url }}">{{ $item->title }}</a>
-          </li>
-        @endforeach
-      </ul>
-    --}}
     @endif
 
-    @if (\App\Site::getMenu('Utility Navigation'))
+    @if (\Kernl\Masthead::getMenu('Utility Navigation'))
       <ul class="__list --utility">
-        @foreach (\App\Site::getMenu('Utility Navigation') as $item)
-          <li class="__item {{ (\App\Site::isActiveMenu('Utility Navigation', $item) ? '--active' : '') }}">
+        @foreach (\Kernl\Masthead::getMenu('Utility Navigation') as $item)
+          <li class="__item {{ (\Kernl\Masthead::isActiveMenu('Utility Navigation', $item) ? '--active' : '') }}">
             <a class="__link" href="{{ $item->url }}">{{ $item->title }}</a>
           </li>
         @endforeach
