6b8c67c62a999644127ebed8a1de5119524699f4
rkingston <r.kingston@northeastern.edu>
2018-03-09 15:12:03 -0500

    more wp template tweaks

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -9,7 +9,10 @@
         @include('templates.single.video')
 
         <div class="row">
-          <div class="col --12@xs --8@lg">
+          <div class="col --12@xs --1@md --1-offset@lg ta--c">
+            @include('templates.single._sharing')
+          </div>
+          <div class="col --12@xs --10@md --8@lg">
             @include('templates.section')
             @include('templates.single.external')
           </div>
---
+++ resources/views/templates/single/_sharing.blade.php
@@ -0,0 +1,31 @@
+@php
+  function url_elements() {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $url = "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+    return urlencode($protocol.$url);
+  }
+  define( 'SHARE_URL', url_elements() );
+@endphp
+
+<ul class="share-buttons">
+  <li>
+    <a href="javascript:PODShare('facebook', '{{ SHARE_URL }}', 555, 350)">
+      <i class="icon--facebook"></i></a>
+    </a>
+  </li>
+  <li>
+    <a href="javascript:PODShare('twitter', '{{ SHARE_URL }}', 520, 442)">
+      <i class="icon--twitter"></i>
+    </a>
+  </li>
+  <li>
+    <a href="javascript:PODShare('linkedin', '{{ SHARE_URL }}', 550, 442)">
+      <i class="icon--briefcase"></i>
+    </a>
+  </li>
+  <li>
+    <a id="podshare_email" href="javascript:PODShare('email', '{{ SHARE_URL }}', 100, 100)">
+      <i class="icon--mail"></i>
+    </a>
+  </li>
+</ul>
