03d72c5ae59b7a2ff02329ad5a9626c71c8b620a
Tim Albert <timothy.m.albert@gmail.com>
2018-05-18 10:59:08 -0400

    Update masthead to look for parent page inheritance

--- resources/views/chrome/masthead.blade.php
+++ resources/views/chrome/masthead.blade.php
@@ -2,19 +2,20 @@
   $option = (is_home() ? 'option' : get_the_ID());
   $class = (get_field('bool_megamenu', 'option') ? ' --megamenu +chevron' : '');
   $class .= (\Kernl\Masthead::getMenu('Utility Navigation') ? ' +utility' : '');
-  $class .= (get_field('bool_customize', $option) && get_field('bool_masthead_overylay', $option) ? ' --overlay' : '');
+  $class .= (get_field('bool_masthead_overylay', $option) || \Kernl\Layout::getParentValues('bool_masthead_overylay') ? ' --overlay' : '');
 @endphp
+{{ \Kernl\Layout::getParentValues('bool_megamenu') }}
 
 <header class="masthead{{ $class }}" role="banner">
   <a class="__logo" href="{{ home_url('/') }}" data-ga-click="Home Logo">
-    <img class="__logo__image" src="{{ (get_field('bool_customize', $option) && get_field('bool_masthead_overylay', $option) ? get_field('med_logo_white','option') : get_field('med_logo','option')) }}" alt="<?= get_bloginfo('name', 'display'); ?> logo">
+    <img class="__logo__image" src="{{ (get_field('bool_customize', $option) && get_field('bool_masthead_overylay', $option) || \Kernl\Layout::getParentValues('bool_masthead_overylay') ? get_field('med_logo_white','option') : get_field('med_logo','option')) }}" alt="<?= get_bloginfo('name', 'display'); ?> logo">
   </a>
   <button class="__toggler hidden--up@lg"><i data-feather="menu"></i></button>
 
   <nav class="__drawer" role="navigation">
     <div class="clearfix hidden--up@lg">
       <a class="__logo" href="{{ home_url('/') }}" data-ga-click="Mobile Drawer Logo">
-        <img class="__logo__image" src="<?= get_field('med_logo_white','option'); ?>" alt="<?= get_bloginfo('name', 'display'); ?> logo">
+        <img class="__logo__image" src="<?= get_field('med_logo_white', 'option'); ?>" alt="<?= get_bloginfo('name', 'display'); ?> logo">
       </a>
       <button class="__toggler menu-is-open"><i data-feather="x"></i></button>
     </div>
