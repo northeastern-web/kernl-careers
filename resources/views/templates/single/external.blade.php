96fc510a14a442d21296806231a871606698ca45
Tim Albert <timothy.m.albert@gmail.com>
2018-05-21 15:09:33 -0400

    Update external for new fields

--- resources/views/templates/single/external.blade.php
+++ resources/views/templates/single/external.blade.php
@@ -1,12 +1,13 @@
-<?php if (get_post_format() == 'link') : ?>
-  <div class="alert m--t-2@xs">
-    <a href="{{ the_field('url_external') }}" class="__link">
-      <div class="__body">
-        <div class="__icon"><i class="ion-log-in"></i></div>
+<?php if (get_field('bool_nunews')) : ?>
+<div class="alert bg--black ta--c mt--2@xs">
+  <a href="{{ the_field('txt_external_url') }}" class="__link">
+    <div class="__body">
+      <div class="__excerpt">
         Originally Published at
         <i>{{ (get_field('bool_nunews') ? 'News@Northeastern' : get_field('text_source')) }}</i>
-        {{ (get_field('text_author') ? 'by ' .get_field('text_author') : '') }}
+        {{ (get_field('txt_author') ? 'by ' .get_field('txt_author') : '') }}
       </div>
-    </a>
-  </div>
+    </div>
+  </a>
+</div>
 <?php endif; ?>
