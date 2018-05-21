c4830a5de66a3932e82f34ad0a45347522464839
Tim Albert <timothy.m.albert@gmail.com>
2018-05-21 15:11:38 -0400

    Clean up single template

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -5,15 +5,16 @@
     @while(have_posts()) @php(the_post())
       @include('templates.banner', ['class' => 'ta--c'])
 
-      <section class="section">
+      <section class="section pb--2@xs">
         @include('templates.single.video')
 
         <div class="row">
-          <div class="col --12@xs --1@md --1-offset@lg ta--c">
+          <div class="col --1@md --1-offset@lg ta--c">
             @include('templates.single._sharing')
           </div>
-          <div class="col --12@xs --10@md --8@lg">
+          <div class="col --10@md --8@lg">
             @include('templates.section')
+
             @include('templates.single.external')
           </div>
         </div>
