564817cbb04abc595cee60d952bfb7cdc322fdec
Tim Albert <timothy.m.albert@gmail.com>
2018-08-07 10:43:09 -0400

    Update common template partial naming

--- resources/views/templates/edit.blade.php
+++
@@ -1,1 +0,0 @@
-@php( edit_post_link('<i class="__icon" data-feather="edit"></i></span><span class="edit-text">Edit ' . (is_single() ? 'Post' : 'Page') . '', '', '', 0, 'post-edit-link btn --sm bg--blue') )
