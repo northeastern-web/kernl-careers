dfa9beb2812c221f927c2281dc25a0fdd7bac718
Tim Albert <timothy.m.albert@gmail.com>
2018-08-07 11:17:32 -0400

    Update external link partial to be a light btn

--- resources/views/templates/single/_external.blade.php
+++ resources/views/templates/single/_external.blade.php
@@ -1,13 +1,11 @@
 @if (get_field('bool_nunews'))
-<div class="alert --sm bg--gray-700 ta--c">
-  <a href="{{ the_field('txt_external_url') }}" class="__link">
-    <div class="__body">
-      <div class="__excerpt">
-        Originally Published at
-        <i>{{ (get_field('bool_nunews') ? 'News@Northeastern' : get_field('text_source')) }}</i>
-        {{ (get_field('txt_author') ? 'by ' .get_field('txt_author') : '') }}
-      </div>
+<a href="{{ the_field('txt_external_url') }}" class="btn --sm --block mb--2 bg--gray-100">
+  <div class="__body">
+    <div class="__excerpt">
+      Originally Published at
+      <i>{{ (get_field('bool_nunews') ? 'News@Northeastern' : get_field('text_source')) }}</i>
+      {{ (get_field('txt_author') ? 'by ' .get_field('txt_author') : '') }}
     </div>
-  </a>
-</div>
+  </div>
+</a>
 @endif
