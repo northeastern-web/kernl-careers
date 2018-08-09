be2d46b3ffadebe1c99d2f1a95b78ada01411936
Tim Albert <timothy.m.albert@gmail.com>
2018-08-09 12:21:10 -0400

    Update card to work properly

--- resources/views/templates/single/card.blade.php
+++ resources/views/templates/single/card.blade.php
@@ -3,9 +3,9 @@
   $has_excerpt = (!empty($hide_excerpt) ? false : true);
 @endphp
 
-<article class="card {{ (isset($class) ? $class : '') }}">
+<article class="card {{ (isset($class) ? $class : '--v') }}">
   <a href="{{ the_permalink() }}" class="__link">
-    @if (get_the_category() && $has_badge)
+    @if(get_the_category() && $has_badge)
       <header class="__header">
         <div class="__column">
           <div class="badge --left {{ (get_field('radio_color', 'category_'.get_the_category()[0]->term_id) ? get_field('radio_color', 'category_'.get_the_category()[0]->term_id) : '') }}">
@@ -15,8 +15,8 @@
       </header>
     @endif
 
-    @if (has_post_thumbnail())
-      <div class="__graphic">
+    @if(has_post_thumbnail())
+      <div class="__graphic ar--16x9">
         <img class="__graphic__img" alt="{{ the_title() }}"
           src="{{ the_post_thumbnail_url('large') }}"
           data-src="{{ the_post_thumbnail_url('large') }}">
@@ -26,10 +26,8 @@
     <section class="__body">
       <h2 class="__title">{{ the_title() }}</h2>
 
-      @if (has_excerpt() && $has_excerpt)
-        {!! get_the_excerpt() !!}
-      @else
-        {!! wp_trim_words(get_the_content(), 20) !!}
+      @if($has_excerpt)
+        {!! (has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 20)) !!}
       @endif
     </section>
   </a>
