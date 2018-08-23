7dd7bc3de1cbf2c0cbd11c8603ca5ad0d5c7f6cd
Tim Albert <timothy.m.albert@gmail.com>
2018-08-23 15:35:29 -0400

    Fix month title

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,13 +1,15 @@
 @extends('chrome.app')
 
 @section('content')
-  @include('templates._banner')
+  @php($class = (is_singular('tribe_events') ? ['class' => '--md'] : ['class' => '--md']))
+  @include('templates._banner', $class)
 
   <div id="tribe-events-pg-template" class="section tribe">
     @if(tribe_is_month())
-      <div class="tribe-month-title">{{ tribe_get_current_month_text() }}</div>
+      @php(tribe_events_before_html())
+      <div class="tribe-month-title tribe-events-page-title">{{ tribe_get_current_month_text() }}</div>
       @php(tribe_get_view())
-
+      @php(tribe_events_after_html())
     @else
       @include('templates.event.'. basename(tribe_get_current_template(), '.php'))
     @endif
