5f7f7bc3d4aed13201cbf5a9c77ef90f684a0709
rkingston <r.kingston@northeastern.edu>
2018-08-28 14:49:26 -0400

    Added !is_singular condition for col--stretch

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   @include('templates._banner', ['class' => '--measure-wide'])
 
-  <div id="tribe-events-pg-template" class="section tribe col--stretch">
+  <div id="tribe-events-pg-template" class="section tribe{{ !is_singular() ? ' col--stretch' : '' }}">
     @if(tribe_is_month())
       @php(tribe_events_before_html())
       <div class="tribe-month-title tribe-events-page-title">{{ tribe_get_current_month_text() }}</div>
