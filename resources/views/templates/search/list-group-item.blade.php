e261483729b5fa8683b4d01a8169d332c202a541
rkingston <r.kingston@northeastern.edu>
2018-10-23 14:30:04 -0400

    Add meta option to search result items

--- resources/views/templates/search/list-group-item.blade.php
+++ resources/views/templates/search/list-group-item.blade.php
@@ -1,7 +1,5 @@
 <a class="__item" href="{{ the_permalink() }}">
   <h6 class="mb--0">{!! the_title() !!}</h6>
-  <div class="text--gray-600 fs--xs pr--1">
-    {!! get_the_excerpt() ? get_the_excerpt() . '<br>' : '' !!}
-    <span class="tc--gray d--block tt--c" style="margin-top: 2px;">{!! (get_post_type() == 'post') ? get_the_category(get_the_ID())[0]->name : get_post_type() !!}</span>
-  </div>
+  {!! get_the_excerpt() ? '<div class="__excerpt text--gray-700 fs--xs pr--1">' . get_the_excerpt() . '</div>' : '' !!}
+  <div class="__meta tt--c">{!! (get_post_type() == 'post') ? get_the_category(get_the_ID())[0]->name : get_post_type() !!}</div>
 </a>
