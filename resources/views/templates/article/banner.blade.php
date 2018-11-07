42935de5a846d2c503d18949e82d0cec5722b555
rkingston <r.kingston@northeastern.edu>
2018-11-07 16:23:10 -0500

    Add ACF; include CPTs and tax in Site class

--- resources/views/templates/article/banner.blade.php
+++ resources/views/templates/article/banner.blade.php
@@ -2,9 +2,9 @@
   <div class="__header">
     <div class="__pretitle">
       <nav class="breadcrumb +icon +chevron mb--0h fs--sm">
-        @foreach (\App\Site::getTaxHierarchy(get_the_ID(), 'group') as $term)
+        {{-- @foreach (\App\Site::getTaxHierarchy(get_the_ID(), 'group') as $term)
           <a class="tc--gray-700" href="{{ get_term_link($term->term_id, 'group') }}">{!! $term->name !!}</a>
-        @endforeach
+        @endforeach --}}
       </nav>
     </div>
     <h1 class="__title">{{ the_title() }}</h1>
