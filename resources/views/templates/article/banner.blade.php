ba464cf5c1606da2e7bb5d2e00de6591194d2a81
Tim Albert <timothy.m.albert@gmail.com>
2018-11-26 16:36:47 -0500

    Rewrite taxonomy hierarchy function

--- resources/views/templates/article/banner.blade.php
+++ resources/views/templates/article/banner.blade.php
@@ -2,9 +2,15 @@
   $option = (is_singular() ? get_the_ID() : get_queried_object());
   $layout = new Kernl\Layout($option);
   $pretitle = '<nav class="breadcrumb +icon +chevron mb--0h fs--sm">';
-  foreach (\App\Site::getTaxHierarchy(get_the_ID(), 'group') as $term) {
-    $pretitle .= '<a class="tc--gray-700" href="' . get_term_link($term->term_id, 'group') . '">' . $term->name . '</a>';
-        }
+  foreach (\App\Site::getTaxTerms(get_the_ID(), 'group') as $term) {
+    if ($term['ancestors']) {
+      foreach (array_reverse($term['ancestors']) as $ancestor) {
+        $pretitle .= '<a class="tc--gray-700" href="' . get_term_link($ancestor->term_id, 'group') . '">' . $ancestor->name . '</a>';
+      }
+    }
+
+    $pretitle .= '<a class="tc--gray-700" href="' . get_term_link($term['item']->term_id, 'group') . '">' . $term['item']->name . '</a>';
+  }
   $pretitle .= '</nav>';
 
   $args = [
