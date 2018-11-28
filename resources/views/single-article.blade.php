0a8e241a967074c23141cb6c63206213846e295f
Tim Albert <timothy.m.albert@gmail.com>
2018-11-28 10:42:41 -0500

    Updated actions

--- resources/views/single-article.blade.php
+++ resources/views/single-article.blade.php
@@ -9,18 +9,6 @@
         <div class="row">
           <div class="col w--2/3@t">
             @include('templates._section')
-
-            <div class="row">
-              <div class="col w--1/2">
-                <h3>Group</h3>
-                <pre>{{ var_dump(\App\Site::getTaxTerms(get_the_ID(), 'group')) }}</pre>
-              </div>
-              <div class="col w--1/2">
-                <h3>Audience</h3>
-                <pre>{{ var_dump(\App\Site::getTaxTerms(get_the_ID(), 'audience')) }}</pre>
-              </div>
-            </div>
-
           </div>
           <div class="col w--1/3@t">
             @include('templates.article._related')
--- resources/views/templates/article/_actions.blade.php
+++ resources/views/templates/article/_actions.blade.php
@@ -1,14 +1,14 @@
-@if(get_field('lay_actions'))
-BLAH
+@if(have_rows('lay_actions'))
   <aside class="card --action bs--none">
     <div class="__header px--1 py--0h bb--1 tt--caps bwa--0 bwb--1 bc--gray-300 fs--xs">
       <div class="__column">Important Actions</div>
     </div>
     <div class="__body mt--2">
       <ul class="fs--sm mb--0">
-        @foreach (get_field('lay_actions') as $action)
-          title
-        @endforeach
+        @while(have_rows('lay_actions'))
+          @php(the_row())
+          {!! the_sub_field('txt_title') !!}
+        @endwhile
       </ul>
     </div>
   </aside>
