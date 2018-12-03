f5c892ba98e51b93e1b56905625c2a91e61567b5
rkingston <r.kingston@northeastern.edu>
2018-12-03 13:59:45 -0500

    Add hero module

---
+++ resources/views/modules/hero.blade.php
@@ -0,0 +1,40 @@
+<?php /** (Hero) */ ?>
+
+<div class="__header px--2">
+  <div class="card bg--black">
+    <div class="__body">
+      <h2 class="fs--d2 d--inline">Find Tools for Your Career</h2>
+      <a class="btn bg--red --sm f--r" href="#">Personalize</a>
+      <form action="" method="GET" class="my--1">
+        <div class="form__enclosed --search --dark bg--gray-500 my--2">
+          <label class="sr--only">Search by keyword</label>
+          <input class="fs--sm" name="s" type="text" placeholder="Search by keyword" autocomplete="off" aria-label="Search" />
+          <button class="btn bg--none bc--black" type="submit">Go</button>
+        </div>
+      </form>
+
+      <h3 class="fs--root --popular">Trending Resources</h3>
+      <div class="row">
+        <div class="col w--50@d">
+          <ul class="ls--none fs--xs ta--c ta--l@d ml--0">
+              <li><a href="#">Etiam Tristique Malesuada Amet</a></li>
+              <li><a href="#">Commodo Sit Tristique Egestas</a></li>
+              <li><a href="#">Bibendum Purus Quam Ridiculus Elit</a></li>
+              <li><a href="#">Lorem Vehicula Aenean Bibendum Pharetra</a></li>
+          </ul>
+        </div>
+        <div class="col w--50@d">
+          <ul class="ls--none fs--xs ta--c ta--l@d">
+              <li><a href="#">Sit Purus Ligula Tellus</a></li>
+              <li><a href="#">Purus Fermentum Euismod Nullam</a></li>
+              <li><a href="#">Tortor Pharetra Nibh Sit Parturient</a></li>
+              <li><a href="#">Aenean Etiam Egestas Ipsum Magna</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="bwa--0 bwt--2 bc--red mt--1 pt--1">
+        <a href="#"><span class="tt--caps">Employers</span>: Hire, train and onboard talent</a>
+      </div>
+    </div>
+  </div>
+</div>
