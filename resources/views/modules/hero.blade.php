a2cc595ccab1ce37aa2afc06a00e5918bb0be487
rkingston <r.kingston@northeastern.edu>
2018-12-14 15:13:23 -0500

    Update hero

--- resources/views/modules/hero.blade.php
+++ resources/views/modules/hero.blade.php
@@ -12,49 +12,14 @@
 @endphp
 
 <div class="__header">
-  <div class="card bg--black mb--0">
-    <div class="__body">
-      <h2 class="fs--d2 d--inline">Design Your Career</h2>
-      <div class="f--r">
-        @if(!$segment)
-          <a class="btn bg--red --sm" href="?college=coe">Personalize</a>
-        @else
-          <div class="fs--sm mt--0h" href="#">
-            <i>Welcome Becky Collet</i> <a href="./" class="tc--gray-500 fs--xs tt--caps fw--300">logout</a>
-          </div>
-        @endif
+  <div class="__body">
+    <h2 class="fs--d4 fw--300">Design Your Tomorrow Today</h2>
+    <form action="" method="GET" class="my--3 w--80@d mx--auto">
+      <div class="form__enclosed --search bg--white-alpha">
+        <label class="sr--only">Search by keyword</label>
+        <input class="fs--sm" name="s" type="text" placeholder="Search by keyword" autocomplete="off" aria-label="Search" />
+        <button class="btn bg--none bc--black" type="submit">Go</button>
       </div>
-      <form action="" method="GET" class="my--1">
-        <div class="form__enclosed --search --dark bg--gray-500 my--2">
-          <label class="sr--only">Search by keyword</label>
-          <input class="fs--sm" name="s" type="text" placeholder="Search by keyword" autocomplete="off" aria-label="Search" />
-          <button class="btn bg--none bc--black" type="submit">Go</button>
-        </div>
-      </form>
-
-      <h3 class="fs--root tt--u">
-        @if(!$segment)
-          Trending Resources
-        @else
-          Resources for <i>Becky Collet</i>
-        @endif
-      </h3>
-      <div class="row">
-        <div class="col w--100@d">
-          <ul class="columns--2 ls--none fs--xs ta--c ta--l@d">
-            @while ($trending->have_posts())
-              @php($trending->the_post())
-              <li class=""><a href="{{ the_permalink() }}">{{ the_title() }}</a></li>
-            @endwhile @php(wp_reset_postdata())
-          </ul>
-        </div>
-      </div>
-
-      @if(!$segment)
-        <div class="bwa--0 bwt--2 bc--red mt--1 pt--1">
-          <a href="{{ get_permalink(137) }}"><span class="tt--caps">Employers</span>: Hire, train and onboard talent</a>
-        </div>
-      @endif
-    </div>
+    </form>
   </div>
 </div>
