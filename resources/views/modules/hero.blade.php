0722bb14579b3bb32b4e11fd55dab01282abbe1d
rkingston <r.kingston@northeastern.edu>
2018-12-18 10:09:47 -0500

    Update hero to new request

--- resources/views/modules/hero.blade.php
+++ resources/views/modules/hero.blade.php
@@ -11,15 +11,16 @@
   ]);
 @endphp
 
-<div class="__header">
-  <div class="__body">
-    <h2 class="fs--d4 fw--300">How can we help you?</h2>
-    <form action="" method="GET" class="my--3 mx--auto">
-      <div class="form__enclosed --search">
-        <label class="sr--only">Search by keyword</label>
-        <input class="fs--sm" name="s" type="text" placeholder="Search by keyword" autocomplete="off" aria-label="Search" />
-        <button class="btn" type="submit">Go</button>
-      </div>
-    </form>
+<form action="" method="GET" class="my--3 mx--auto">
+  <div class="form__enclosed --search">
+    <label class="sr--only">Search by keyword</label>
+    <input class="fs--sm" name="s" type="text" placeholder="Search by keyword" autocomplete="off" aria-label="Search" />
+    <button class="btn" type="submit">Go</button>
   </div>
+</form>
+
+<div class="row">
+  <div class="col w--1/3@t mb--1"><a class="btn --m tt--caps --right" href="./students/">Students <i data-feather="arrow-right"></i></a></div>
+  <div class="col w--1/3@t mb--1"><a class="btn --m tt--caps --right" href="./employers/">Employers <i data-feather="arrow-right"></i></a></div>
+  <div class="col w--1/3@t mb--1"><a class="btn --m tt--caps --right" href="./alumni/">Alumni <i data-feather="arrow-right"></i></a></div>
 </div>
